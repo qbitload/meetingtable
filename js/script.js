f33ca8286cff77bbb2e1890c7bdcd651fe2b2113
John .Y <aouir.yahia@gmail.com>
2021-02-05 08:34:11 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// var scene, camera, renderer;
+
+// scene = new THREE.Scene();
+// scene.background = new THREE.Color(0x5f5f0f);
+
+// camera = new THREE.PerspectiveCamera(50,window.innerWidth/window.innerHeight);
+// camera.position.set(0,100,1000);
+// renderer = new THREE.WebGLRenderer();
+// // renderer.setSize(window.innerWidth,window.innerHeight);
+// renderer.setSize(window.innerWidth,window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+
+
+
+// var controls = new THREE.OribitControls(camera,renderer.domElement);
+// controls.update(); 
+
+// var abint = new THREE.AmbientLight(0x555500)
+// scene.add(abint)
+
+// var loader = new THREE.GLTFLoader();
+
+// loader.load( 'scene.gltf', function ( gltf ) {
+
+//     scene.add( gltf.scene );
+    
+// } );
+
+
+// function animate(){
+
+//     requestAnimationFrame(animate);
+//     renderer.render(scene,camera);
+// }
+
+// animate()
+
+
+
